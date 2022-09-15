607db547c98fca00095c736de704a2054e4c26e8
susan749 <susanzelaya15@gmail.com>
2022-09-14 19:30:03 -0600

    Agregar archivos de proyecto.

---
+++ DSE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
